be4899536ac90f21dc18ec586107c17888b5f8df
Arjan Scherpenisse <arjan@scherpenisse.net>
2020-10-04 09:58:33 +0200

    Change target

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Radio.MixProject do
 
   @app :radio
   @version "0.1.0"
-  @all_targets [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :rpi4, :bbb, :x86_64]
+  @all_targets [:hifiberry_rpi0]
 
   def project do
     [
@@ -49,14 +49,7 @@ defmodule Radio.MixProject do
       {:nerves_pack, "~> 0.4.0", targets: @all_targets},
 
       # Dependencies for specific targets
-      {:nerves_system_rpi, "~> 1.12", runtime: false, targets: :rpi},
-      {:nerves_system_rpi0, "~> 1.12", runtime: false, targets: :rpi0},
-      {:nerves_system_rpi2, "~> 1.12", runtime: false, targets: :rpi2},
-      {:nerves_system_rpi3, "~> 1.12", runtime: false, targets: :rpi3},
-      {:nerves_system_rpi3a, "~> 1.12", runtime: false, targets: :rpi3a},
-      {:nerves_system_rpi4, "~> 1.12", runtime: false, targets: :rpi4},
-      {:nerves_system_bbb, "~> 2.7", runtime: false, targets: :bbb},
-      {:nerves_system_x86_64, "~> 1.12", runtime: false, targets: :x86_64},
+      {:hifiberry_rpi0, path: "../hifiberry_rpi0", runtime: false, targets: :hifiberry_rpi0}
     ]
   end
 
